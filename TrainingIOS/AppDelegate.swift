2e89557120d8ceb36c17d92570aa8aa12eeaf4df
DaoNhat <daonhat@DaoNhat-no-MacBook-Air.local>
2017-08-07 15:37:49 +0700

    Screen new Status

--- TrainingIOS/AppDelegate.swift
+++ TrainingIOS/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        registerNotification()
         return true
     }
 
@@ -39,6 +40,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func registerNotification() {
+            let notifSettings = UIUserNotificationSettings.init(
+            types: [.sound, .alert, .badge],
+            categories: nil
+        )
+        UIApplication.shared.registerUserNotificationSettings(notifSettings)
+        UIApplication.shared.registerForRemoteNotifications()
+        
     }
 
 
